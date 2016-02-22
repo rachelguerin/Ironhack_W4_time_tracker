665473925b0f883f790af173eee9516180b12bde
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-22 18:05:27 +0100

    refactor - add last_created_projects and name_with_description methods to project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,12 @@ class Project < ActiveRecord::Base
 	def self.clean_old
 		where("created_at < ?",Time.now - 1.week).destroy_all
 	end
+
+	def self.last_created_projects(num)
+		order(created_at: :desc).limit(num)
+	end
+
+	def name_with_description 
+		self.name += self.description ? "- #{self.description}" : ""
+	end
 end
