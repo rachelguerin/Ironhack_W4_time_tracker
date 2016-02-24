756c2647fc87e3e0885fc165c8db743433b92af1
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-24 15:06:10 +0100

    display err messages

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,6 +13,19 @@ class EntriesController < ApplicationController
 	end
 
 	def create
+		@project = Project.find_by(id: params[:project_id])
+		@entry = @project.entries.new entry_params
+		if @entry.save
+			redirect_to action: 'index', controller: 'entries', project_id: @project.id
+		else 
+			render "new"
+		end
+	
+	end
 
+	private
+
+	def entry_params
+		params.require(:entry).permit(:hours, :minutes, :date)
 	end
 end
